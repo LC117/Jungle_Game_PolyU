b30f1217fe99c00e6df51dfcdcf7a317c8f492a6
Luca <Scalerandi.Luca@gmail.com>
2019-09-24 16:02:12 +0800

    Update GameBoard.java
    1)toString() Method is implemented and working
    2) The board game is now a Class and created in the constructor

--- src/hk/edu/polyu/comp/comp2021/jungle/model/GameBoard.java
+++ src/hk/edu/polyu/comp/comp2021/jungle/model/GameBoard.java
@@ -3,53 +3,50 @@ package hk.edu.polyu.comp.comp2021.jungle.model;
 import java.util.Arrays;
 
 public class GameBoard {
+    private static int height = 9;
+    private static int width = 7;
+    private String [][] boardArray = new String [height][width]; // y, x
 
-    private String[][] createBoard(){
-        int height = 9;
-        int width = 7;
-
-        String [][]  board = new String [height][width]; // y, x
-
+    public GameBoard() {
         for(int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 if (y == 0 || y == height - 1) {
                     if (x == 3) {
-                        board[y][x] = "{_}"; // den
+                        boardArray[y][x] = "{_}"; // den
                     } else if (x == 2 || x == 4) {
-                        board[y][x] = "[_]"; //traps
+                        boardArray[y][x] = "[_]"; //traps
                     }else{
-                        board[y][x] = "(_)"; //water
+                        boardArray[y][x] = "|_|"; //water
                     }
                 } else if (y == 1 || y == height - 2) {
                     if (x == 3) {
-                        board[y][x] = "[_]";
+                        boardArray[y][x] = "[_]";
                     } else {
-                        board[y][x] = "(_)";
+                        boardArray[y][x] = "|_|";
                     }
                 } else if (y == 3 || y == 4 || y == 5) {
-                    if (x == 1 || x == 2 || x == width - 1 || x == width - 2) {
-                        board[y][x] = "(_)";
+                    if (x == 1 || x == 2 || x == width - 2 || x == width - 3) {
+                        boardArray[y][x] = "(_)";
                     }else{
-                        board[y][x] = "(_)";
+                        boardArray[y][x] = "|_|";
                     }
                 } else {
-                    board[y][x] = "|_|";
+                    boardArray[y][x] = "|_|";
                 }
             }
         }
-        return board;
     }
     @Override
     public String toString(){
-        String board = "";
+        String boardString = "";
         for (int i = 0; i < 9; i++) {
-
+            boardString += Arrays.toString(this.boardArray[i]) + "\n";
         }
-        return "";
+        return boardString;
     }
     public static void main (String []args){
         GameBoard g = new GameBoard();
-        System.out.println(Arrays.deepToString(g.createBoard()));
+        System.out.println(g.toString());
     }
 }
 
