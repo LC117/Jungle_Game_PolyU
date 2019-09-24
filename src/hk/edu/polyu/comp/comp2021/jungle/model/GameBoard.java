d1ebbfd7567aac0792db7d7772a954ddabf68108
kramsphi <54886026+kramsphi@users.noreply.github.com>
2019-09-24 14:56:12 +0800

    Update GameBoard.java
    added semicolons

--- src/hk/edu/polyu/comp/comp2021/jungle/model/GameBoard.java
+++ src/hk/edu/polyu/comp/comp2021/jungle/model/GameBoard.java
@@ -11,23 +11,23 @@ public class GameBoard {
             for(int x = 0; x < width; x++) {
                 if (y == 0 || y == height -1) {
                     if(x == 3){
-                        board[y][x] == "{_}"
+                        board[y][x] == "{_}";
                     }
                     elif(x==2 || x==4){
-                        board[y][x] == "[_]"
+                        board[y][x] == "[_]";
                     }
                 else if(y == 1 || y == height -2) {
                     if(x==3) {
-                        board[y][x] == "[_]"
+                        board[y][x] == "[_]";
                     }
                 }
                 else if(y==3 || y == 4 || y ==5){
                     if(x==1 || x==2 || x==width -1 || x==width -2){
-                        board[y][x] == "(_)"
+                        board[y][x] == "(_)";
                     }
                 }
                 else{
-                    board[y][x] == "|_|"
+                    board[y][x] == "|_|";
                 }
 
 
