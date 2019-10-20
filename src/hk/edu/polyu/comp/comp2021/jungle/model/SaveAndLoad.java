969af912f41ac6c3617df4db494fc2762a25e16a
Luca <Scalerandi.Luca@gmail.com>
2019-10-20 15:52:04 +0800

    SaveAndLoad update

--- src/hk/edu/polyu/comp/comp2021/jungle/model/SaveAndLoad.java
+++ src/hk/edu/polyu/comp/comp2021/jungle/model/SaveAndLoad.java
@@ -1,6 +1,15 @@
 package hk.edu.polyu.comp.comp2021.jungle.model;
 
 import org.json.*;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 //TODO: should me in model!
 public class SaveAndLoad {
 
@@ -8,10 +17,19 @@ public class SaveAndLoad {
 
     }
     private void saveGame (String path){
+        File file = new File(path);
+
         JSONObject jo = new JSONObject();
     }
 
     public static void main(String[] args) {
-    //to test!
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        try {
+            List<String> lineList = Files.readAllLines(Paths.get("Z:\\Programming\\java_test\\test.txt")); //Works with Path!!
+            System.out.println(Arrays.toString(lineList.toArray()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
