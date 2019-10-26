d934569745f04e6ada5b9c2518eaeff1f0867611
Luca <Scalerandi.Luca@gmail.com>
2019-10-26 13:44:32 +0800

    first Unitest of View implemented

--- test/hk/edu/polyu/comp/comp2021/jungle/view/ViewTest.java
+++ test/hk/edu/polyu/comp/comp2021/jungle/view/ViewTest.java
@@ -1,16 +1,39 @@
 package hk.edu.polyu.comp.comp2021.jungle.view;
 
+import hk.edu.polyu.comp.comp2021.jungle.model.GameBoard;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.PrintStream;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ViewTest {
+    private static ByteArrayOutputStream outPutContent;
+
+    @BeforeEach
+    public void setUpTestStreams(){
+        outPutContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outPutContent));
+    }
+
 
     @Test
     void displayMessage() {
+        String testMessage = "This is a test message! \n";
+        View view = new View();
+        view.displayMessage(testMessage);
+        assertEquals(testMessage + System.getProperty("line.separator"), outPutContent.toString(),
+                "Tests if view.displayMessage() prints a test message correctly.");
     }
 
     @Test
     void displayGameUpdate() {
+        GameBoard testGameBoard = new GameBoard();
+        View view = new View();
+        view.displayGameUpdate(testGameBoard);
+        assertEquals(testGameBoard.toString() + System.getProperty("line.separator"), outPutContent.toString(),
+                "Tests if displayGameUpdate() correctly prints a GameBoard object.");
     }
 }
